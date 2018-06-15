b70b5e92f17a8560b1336ef4fe2f561859e6aa94
arturfracala <arturfracala@gmail.com>
2018-06-15 12:04:57 +0200

    Update zegar.asm

--- zegar.asm
+++ zegar.asm
@@ -3,12 +3,12 @@ write_data EQU 0FF2DH
 read_status EQU	0FF2EH
 read_data EQU 0FF2CH
 
-init EQU 038H					; Kod inicjalizacji wyswietlacza
-clear EQU 001H					; Kod wyczyszczenia wyswielacza
-on	EQU 00EH 					; Kod wlaczenia lcd
+init EQU 038H					
+clear EQU 001H					
+on	EQU 00EH 					
 
-TIME EQU 50 					; Zmienna - czas do odmierzenia w ms
-LOAD EQU (65536 - TIME*1000) 	; Zmienna - liczba cykli potrzebnych do odmierzenia czasu 
+TIME EQU 50 					
+LOAD EQU (65536 - TIME*1000) 	
 
 SEC EQU 020H
 MIN EQU 028H
@@ -25,11 +25,10 @@ ORG 0
 
 	SJMP	loop
 
-ORG 0BH 						; Blok kodu obslugi przerwania, start pod adresem 0x00BH
+ORG 0BH 						
 
-    MOV TH0, #HIGH(LOAD) 		; Zaladowanie starszych bitów zmiennej z liczba cykli(stop)
-    MOV TL0, #LOW(LOAD) 		; Zaladowanie mlodszych bitów zmiennej z liczba cykli(start)
-
+    	MOV TH0, #HIGH(LOAD) 		
+   	MOV TL0, #LOW(LOAD) 		
 
 	PUSH PSW
 	PUSH ACC
@@ -38,7 +37,7 @@ ORG 0BH 						; Blok kodu obslugi przerwania, start pod adresem 0x00BH
 	DJNZ LICZNIK, int_exit
 	MOV LICZNIK, #20 
 
-    CPL P1.0 					; Zapalanie/zgaszenie diody na linii 0 portu P1
+    	CPL P1.0 					
 
 	SETB SEG_CHANGE
 
@@ -60,7 +59,7 @@ ORG 0BH 						; Blok kodu obslugi przerwania, start pod adresem 0x00BH
 int_exit:
 	POP ACC
 	POP PSW
-    RETI ; Wyjscie z bloku kodu obslugi przerwania
+    RETI 
 
 loop:
 	LCALL time_display
@@ -71,54 +70,54 @@ loop:
 work:
 	SJMP	loop
 				
-;--------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 ; Zapis komendy		A - kod komendy
-;--------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 lcd_write_cmd:
 	PUSH ACC
-	MOV DPTR, #read_status		;Wartosc adresu pamieci do dptra
+	MOV DPTR, #read_status		
 
 petla_zajetosci_cmd:
-	MOVX A, @DPTR					;Zawartosc z adresu read_status wkladamy do akumulatora
-	JB ACC.7, petla_zajetosci_cmd   ;Na 6 bicie jest ostatni adres, a na 7 jest BF. Musimy sprawdzic, czy jest zajeta
+	MOVX A, @DPTR					
+	JB ACC.7, petla_zajetosci_cmd 
 	
 	MOV DPTR, #write_cmd
 	POP ACC
 	MOVX @DPTR, A
 
 	RET
-;--------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 ; Inicjalizacja wyswietlacza
-;--------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 lcd_inicjalizacja:
-	MOV A, #init				  		; Do akumulatora kod inicjalizujacy wyswietlacz
-	CALL lcd_write_cmd			  		; Wywolanie funckji zapisu komendy - dla init
-	MOV A, #clear			   	  		; Czyszczenie lcd
-	CALL lcd_write_cmd			  		; Wywolanie czyszczenia
-	MOV A, #on				   	  		; Do akumulatora wpisujmey kod wlaczajacy wyswietlacz lcd
-	CALL lcd_write_cmd			  		; Wywolujemy wlaczenie lcd
+	MOV A, #init				  		
+	CALL lcd_write_cmd			  		
+	MOV A, #clear			   	  		
+	CALL lcd_write_cmd			  		
+	MOV A, #on				   	  		
+	CALL lcd_write_cmd			  		
 	RET
 
-;--------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 ; Wyswietlanie danych		A - kod wyswietlania danych w lcd 
-;--------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 lcd_write_data:
 	PUSH ACC
-	MOV DPTR, #read_status				; Wartosc adresu pamieci do dptra
+	MOV DPTR, #read_status				
 
  petla_zajetosci_danych:
-	MOVX A, @DPTR						; Zawartosc z adresu read_status wkladamy do akumulatora
-	JB ACC.7, petla_zajetosci_danych    ; Na 6 bicie jest ostatni adres, a na 7 jest BF. Musimy sprawdzic, czy jest zajeta
+	MOVX A, @DPTR						
+	JB ACC.7, petla_zajetosci_danych    
 
 	MOV DPTR, #write_data
 	POP ACC
-	MOVX @DPTR, A		  				; Musimy to odzyskaæ, bo w A bylyby smieci
+	MOVX @DPTR, A		  				
 
 	RET
 
-;--------------------------------------------
-; Wypisywanie liczby zapisanej na dwoch miejscach 
-;--------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
+; Wypisywanie liczby zapisanej na dwoch pozycjach 
+;------------------------------------------------------------------------------------------------------------------------------------
 lcd_wyswietl_liczbe:
 	MOV B, #10
 	DIV AB
@@ -129,9 +128,9 @@ lcd_wyswietl_liczbe:
 	CALL lcd_write_data
 	RET
 
-;---------------------------------------------------------------------------------------------
-; Realizacja procedury inicjujacej timer wykorzystujacy mechanizm przerwan
-;---------------------------------------------------------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
+; Realizacja procedury inicjujacej timer wykorzystujacy mechanizmu przerwan
+;------------------------------------------------------------------------------------------------------------------------------------
 INICJUJ_TIMER:
 	MOV SEC, #0
 	MOV MIN, #0
@@ -139,24 +138,24 @@ INICJUJ_TIMER:
 	MOV LICZNIK, #20 
 
 
-    CLR TR0 				; Wylaczanie timera
-    ANL TMOD, #0F0H 		; Wyzerowanie rejestru sterujacego timera nr 0
-    ORL TMOD, #1 			; Ustawienie trybu timera na 16 bitów
+    	CLR TR0
+    	ANL TMOD, #0F0H 		
+    	ORL TMOD, #1 			
 
-    MOV TH0, #HIGH(LOAD) 	; Zaladowanie starszych bitów zmiennej z liczba cykli(stop)
-    MOV TL0, #LOW(LOAD) 	; Zaladowanie mlodszych bitów zmiennej z liczba cykli(start)
-    CLR TF0 				; Wyzerowanie bitu przepelnienia
+    	MOV TH0, #HIGH(LOAD) 	
+    	MOV TL0, #LOW(LOAD) 	
+    	CLR TF0 				
 
-    SETB ET0 				; Ustawienie obslugi przerwania dla timera nr 0
-    SETB EA 				; Ustawienie obslugi przerwania
-    SETB TR0 				; Wlaczenie timera
+    	SETB ET0 				
+    	SETB EA 				
+    	SETB TR0 				
 
-    RET 					; Wyjscie z procedury
+    RET 					
 
 
-;---------------------------------------------------------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 ; Wyswietlanie czasu
-;---------------------------------------------------------------------------------------------
+;------------------------------------------------------------------------------------------------------------------------------------
 time_display:
 	MOV A, 00000000B
 	CALL  gotoxy
@@ -178,21 +177,21 @@ time_display:
 RET
 
 gotoxy:
-	 MOV R2, #0				; Zerwanie rejestru r2
-	 JNB ACC.4, next		; Jesli flaga BF next
+	 MOV R2, #0			
+	 JNB ACC.4, next		
 	 MOV R2, #1
 					  	
 	 next:
-	 MOV R3, #00001111b		; Maska na 4 ost bity
-	 ANL A, R3				; Iloczyn logiczny maski z akumulatorem, bierzemy pod uwage tylko 4 ostatnie bity
+	 MOV R3, #00001111b		
+	 ANL A, R3				
 	 MOV R3, A
 	 
-	 MOV A, R2				; Wpisujemy bit 0 - linia y do A
-	 MOV B, #01000000b		; Przesanie 64 - liczby bitow w pojedynczej linii lcd
-	 MUL AB					; Wyznaczenie adresu paiemieci dla y - wyznaczenie miejsca pamieci, w ktorym jest y
-     ADD A, R3  			; Dodanie do akumulatora wsp x
-	 MOV A, #10000000b		; Wlaczanie adresowanie pamieci XRAM DDRAM		
-	 CALL lcd_write_cmd		; Wywolanie zapisu 	
+	 MOV A, R2				
+	 MOV B, #01000000b		
+	 MUL AB				
+     	 ADD A, R3  			
+	 MOV A, #10000000b				
+	 CALL lcd_write_cmd			
 
 	RET
 END 
